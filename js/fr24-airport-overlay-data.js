b7929e786620de29a8dfcd3cdeb5aea764d13228
VirtualVelocity <djstudios2000@gmail.com>
2017-05-13 18:10:18 +1000

    Fix KLAX

--- js/fr24-airport-overlay-data.js
+++ js/fr24-airport-overlay-data.js
@@ -1,7 +1,7 @@
 var fr24_airport_overlay_data = [
     {
 		neBoundLat: 33.953684,
-		neBoundLng: -118.384809,
+		neBoundLng: -118.584809,
 		swBoundLat: 33.925903,
 		swBoundLng: -118.440020,
 		name: "KLAX",
