bc8287e0aef1affe4edcaf246ca9c1e1e579ac42
VirtualVelocity <djstudios2000@gmail.com>
2017-05-13 17:32:04 +1000

    First Upload

---
+++ js/fr24-airport-overlay-data.js
@@ -0,0 +1,28 @@
+var fr24_airport_overlay_data = [
+    {
+		neBoundLat: 33.959868,
+		neBoundLng: -118.378179,
+		swBoundLat: 33.925903,
+		swBoundLng: -118.440020,
+		name: "KLAX",
+		IATA: "LAX",
+		ICAO: "KLAX",
+		uploader: "JScott",
+		email: "",
+		date: 2017,
+		image: "KLAX.png"
+	},
+	{
+		neBoundLat: -33.92273053019009,
+		neBoundLng: 151.20755110740658,
+		swBoundLat: -33.9812189189758,
+		swBoundLng: 151.14664332389827,
+		name: "Sydney Airport",
+		IATA: "SYD",
+		ICAO: "YSSY",
+		uploader: "mkyung",
+		email: "",
+		date: 2014,
+		image: "YSSY.png"
+	},
+	]
